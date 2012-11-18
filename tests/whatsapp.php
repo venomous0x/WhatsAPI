8e09d11af8139e68f8f200884a46ee81cc7dbd0b
mohamedalii99 <mestekanias_onlie@hotmail.com>
2012-11-18 02:20:11 +0200

    Update tests/whatsapp.php

--- tests/whatsapp.php
+++ tests/whatsapp.php
@@ -14,8 +14,8 @@ function fgets_u($pStdn) {
 }
 
 $nickname = "WhatsAPI Test";
-$sender = 	""; // Mobile number with country code (but without + or 00)
-$imei = 	""; // MAC Address for iOS IMEI for other platform (Android/etc) 
+$sender = 201019562811	""; // Mobile number with country code (but without + or 00)
+$imei = 012651006989673	""; // MAC Address for iOS IMEI for other platform (Android/etc) 
 
 
 $countrycode = substr($sender, 0, 2);
