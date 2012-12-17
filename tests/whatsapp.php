096d0edecb64f83559d8e1965ce3609a91ab5648
Maantwomaan <antonius.energy@hotmail.de>
2012-12-17 17:26:14 +0100

    Update tests/whatsapp.php

--- tests/whatsapp.php
+++ tests/whatsapp.php
@@ -14,8 +14,8 @@ function fgets_u($pStdn) {
 }
 
 $nickname = "WhatsAPI Test";
-$sender = 	""; // Mobile number with country code (but without + or 00)
-$imei = 	""; // MAC Address for iOS IMEI for other platform (Android/etc) 
+$sender = 	"4915778202470"; // Mobile number with country code (but without + or 00)
+$imei = 	"353975058345113"; // MAC Address for iOS IMEI for other platform (Android/etc) 
 
 
 $countrycode = substr($sender, 0, 2);
