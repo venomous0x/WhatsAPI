081cc90f5ffb198bc0e7d41606c60a6efc504ab3
Swen Kooij <nuser1@live.com>
2013-02-11 17:47:05 +0100

    Added code_confirm and exists functions

--- src/php/registration.php
+++ src/php/registration.php
@@ -58,4 +58,96 @@
 		// Return response
 		return json_decode($Response);
 	}
+
+	/*
+		Confirm the code received 
+	*/
+	function confirm_code($cc, $phone, $code, $id="")
+	{
+		// User agent - WARNING! Do not change unless you also change the TokenPrefix!
+		$UserAgent = "WhatsApp/2.3.53 S40Version/14.26 Device/Nokia302";
+	
+		// Build token
+		$TokenPrefix = 'PdA2DJyKoUrwLw1Bg6EIhzh502dF9noR9uFCllGk1354754753509';
+		$Token = $TokenPrefix . $phone;
+		$Token = md5($Token);
+
+		// Create md5 hash of id (Note: If no id has been given, a hash needs to be created from an empty string
+		$id = md5($id);
+
+		// Build url
+		$Url = "v.whatsapp.net/v2/register?cc=$cc&in=$phone&id=$id&code=$code";
+
+		// Set useragent and Accept header
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_HEADER, 0);
+		curl_setopt($ch, CURLOPT_USERAGENT, $UserAgent);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept: text/json'));
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // WARNING: Little hack here! This makes CURL accept any peer!
+		
+		// Get response
+		$Response = curl_exec($ch);
+		
+		// Close response
+		curl_close($ch);
+		
+		// Check for errors
+		if(strpos($Response, '"missing_param","param":"c"') || strpos($Response, '"bad_param","param":"c"'))
+		{
+			throw new Exception('The request that was made was not an HTTPS request.');
+		}
+		else if(strpos($Response, 'You have received a WhatsApp authorization code'))
+		{
+			throw new Exception('The request that was made was not an HTTPS request and the parameter \'c\' was given.');
+		}
+		
+		// Return response
+		return json_decode($Response);
+	}
+
+	/*
+		Check if a phone number exiss, if true the response will contain a new password
+	*/
+	function exists($cc, $phone, $id="")
+	{
+		// User agent - WARNING! Do not change unless you also change the TokenPrefix!
+		$UserAgent = "WhatsApp/2.3.53 S40Version/14.26 Device/Nokia302";
+	
+		// Build token
+		$TokenPrefix = 'PdA2DJyKoUrwLw1Bg6EIhzh502dF9noR9uFCllGk1354754753509';
+		$Token = $TokenPrefix . $phone;
+		$Token = md5($Token);
+
+		// Create md5 hash of id (Note: If no id has been given, a hash needs to be created from an empty string
+		$id = md5($id);
+
+		// Build url
+		$Url = "v.whatsapp.net/v2/exists?cc=$cc&in=$phone&id=$id";
+
+		// Set useragent and Accept header
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_HEADER, 0);
+		curl_setopt($ch, CURLOPT_USERAGENT, $UserAgent);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept: text/json'));
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // WARNING: Little hack here! This makes CURL accept any peer!
+		
+		// Get response
+		$Response = curl_exec($ch);
+		
+		// Close response
+		curl_close($ch);
+		
+		// Check for errors
+		if(strpos($Response, '"missing_param","param":"c"') || strpos($Response, '"bad_param","param":"c"'))
+		{
+			throw new Exception('The request that was made was not an HTTPS request.');
+		}
+		else if(strpos($Response, 'You have received a WhatsApp authorization code'))
+		{
+			throw new Exception('The request that was made was not an HTTPS request and the parameter \'c\' was given.');
+		}
+		
+		// Return response
+		return json_decode($Response);
+	}
 ?>
