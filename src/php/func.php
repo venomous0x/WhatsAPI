74a4ae02cf63a23100d195c7af937f452aacc580
facine <manu@facine.es>
2013-02-18 15:24:49 +0100

    Remove unused functions in func.php.

--- src/php/func.php
+++ src/php/func.php
@@ -1,43 +1,5 @@
 <?php
 
-function isShort($str)
-{
-    $len = strlen($str);
-    if ($len < 256) {
-        $res = TRUE;
-    } else {
-        $res = FALSE;
-    }
-
-    return $res;
-}
-
-function strlen_wa($str)
-{
-    $len = strlen($str);
-    if ($len >= 256) {
-        $len = $len&0xFF00 >> 8;
-    }
-
-    return $len;
-}
-
-function _hex($int)
-{
-    return (strlen(sprintf("%X", $int)) %2 == 0) ? sprintf("%X", $int) : sprintf("0%X", $int);
-}
-
-function random_uuid()
-{
-    return sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
-        mt_rand(0, 0xffff), mt_rand(0, 0xffff),
-        mt_rand(0, 0xffff),
-        mt_rand(0, 0x0fff) | 0x4000,
-        mt_rand(0, 0x3fff) | 0x8000,
-        mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff)
-    );
-}
-
 function pbkdf2($algorithm, $password, $salt, $count, $key_length, $raw_output = FALSE)
 {
     $algorithm = strtolower($algorithm);
@@ -66,31 +28,6 @@ function pbkdf2($algorithm, $password, $salt, $count, $key_length, $raw_output =
     } else {
         return bin2hex(substr($output, 0, $key_length));
     }
-}
-
-function strtohex($str)
-{
-    $hex = '';
-    for ($i=0; $i < strlen($str); $i++) {
-        $hex .= "\x".dechex(ord($str[$i]));
-    }
-
-    return $hex;
-}
-
-function startsWith($haystack, $needle , $pos=0)
-{
-    $length = strlen($needle);
-
-    return (substr($haystack, $pos, $length) === $needle);
-}
-
-function endsWith($haystack, $needle)
-{
-    $length = strlen($needle);
-    $start  = $length * -1;
-
-    return (substr($haystack, $start) === $needle);
 }
 
 function createIcon($file)
