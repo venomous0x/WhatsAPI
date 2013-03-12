3ca2a12124beb0baba1e240896998e2b39085b03
ginunez <ginunez@gmail.com>
2013-03-12 13:49:37 -0300

    Update whatsapp.php

--- tests/whatsapp.php
+++ tests/whatsapp.php
@@ -16,8 +16,8 @@ function fgets_u($pStdn)
 }
 
 $nickname = "WhatsAPI Test";
-$sender = 	""; // Mobile number with country code (but without + or 00)
-$imei = 	""; // MAC Address for iOS IMEI for other platform (Android/etc)
+$sender = 	"56962060169"; // Mobile number with country code (but without + or 00)
+$imei = 	"353718055231971"; // MAC Address for iOS IMEI for other platform (Android/etc)
 
 if ($argc < 2) {
     echo "USAGE: ".$_SERVER['argv'][0]." [-l] [-s <phone> <message>] [-i <phone>] [-set <status>]\n";
