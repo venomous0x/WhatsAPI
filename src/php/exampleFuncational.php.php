d397f42997955a02d97210da975179a430b8b30b
Jonathan <Jonathan@Viper>
2013-07-10 22:10:58 +0100

    Created two new example files to show how to either send messages once you know your password, or how to register to get your password.

---
+++ src/php/exampleFuncational.php.php
@@ -0,0 +1,98 @@
+<?php
+set_time_limit(10);
+require_once 'whatsprot.class.php';
+require_once 'contacts.php';
+
+
+echo "<textarea cols='170' rows='50'>";
+
+// phone number, deviceIdentity, and name.
+$options = getopt("d::", array("debug::"));
+$debug = (array_key_exists("debug", $options) || array_key_exists("d", $options)) ? true : false;
+
+$username = "**your phone number**";                    // Telephone number including the country code without '+' or '00'.
+$identity = "**server generated whatsapp deviceID.**";  // Obtained during registration with this API or using MissVenom (https://github.com/shirioko/MissVenom)to sniff from your phone.
+$password = "**server generated whatsapp password**";   // A server generated Password you received from WhatsApp. This can NOT be manually created
+$nickname = "**your nickname**";                        // This is the username (or nickname) displayed by WhatsApp clients.
+$target = "**contact's phone number**";                 // Destination telephone number including the country code without '+' or '00'.
+
+
+//Create the whatsapp object and setup a connection.
+$w = new WhatsProt($username, $identity, $nickname, $debug);
+$w->connect();
+// Now LoginWithPassword function sends Nickname and (Available) Presence
+$w->loginWithPassword($password);
+
+//retrieve large profile picture/ output is in /src/php/pictures/
+$w->sendGetProfilePicture($target, true);
+
+//update your profile picture
+$w->sendSetProfilePicture("demo/venom.jpg");
+
+//send picture
+$w->sendMessageImage($target, "demo/x3.jpg");
+
+//send video
+//$w->sendMessageVideo($target, 'http://example.com/video.mp4');
+
+//send Audio
+//$w->sendMessageAudio($target, 'http://example.com/audio.3gp');
+
+//send Location
+//$w->sendLocation($target, '4.948568', '52.352957');
+
+
+
+// Implemented out queue messages and auto msgid
+$w->sendMessage($target, "Sent from WhatsApi at " . time());
+
+/**
+ * You can create a ProcessNode class (or whatever name you want) that has a process($node) function
+ * and pass it through setNewMessageBind, that way everytime the class receives a text message it will run
+ * the process function to it.
+ */
+$pn = new ProcessNode($w, $target);
+$w->setNewMessageBind($pn);
+
+while (1) {
+    $w->pollMessages();
+    $msgs = $w->getMessages();
+    foreach ($msgs as $m) {
+        # process inbound messages
+        //print($m->NodeString("") . "\n");
+    }
+}
+
+/**
+ * Demo class to show how you can process inbound messages
+ */
+class ProcessNode
+{
+    protected $wp = false;
+    protected $target = false;
+
+    public function __construct($wp, $target)
+    {
+        $this->wp = $wp;
+        $this->target = $target;
+    }
+
+    public function process($node)
+    {
+        // Example of process function, you have to guess a number (psss it's 5)
+        // If you guess it right you get a gift
+        $text = $node->getChild('body');
+        $text = $text->data;
+        if ($text && ($text == "5" || trim($text) == "5")) {
+            $iconfile = "../../tests/Gift.jpgb64";
+            $fp = fopen($iconfile, "r");
+            $icon = fread($fp, filesize($iconfile));
+            fclose($fp);
+            $this->wp->sendMessageImage($this->target, "https://mms604.whatsapp.net/d11/26/09/8/5/85a13e7812a5e7ad1f8071319d9d1b43.jpg", "hero.jpg", 84712, $icon);
+            $this->wp->sendMessage($this->target, "Congratulations you guessed the right number!");
+        } else {
+            $this->wp->sendMessage($this->target, "I'm sorry, try again!");
+        }
+    }
+
+}
---
+++ src/php/exampleRegister.php.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * This is an example of how you can use the WhatsAPI to request a code
+ * from the WhatsAPP server, register that code and retrieve your password.
+ *
+ * Once you have your password you will then be able to use it in
+ * functionalexample.php to actually send and receive messages.
+ *
+ */
+
+
+require_once('whatsprot.class.php');
+
+/**
+ * Config data.
+ */
+$debug = true;
+
+$username = '34666554433';                      // Telephone number including the country code without '+' or '00'.
+$identity = 'e807f1fcf82d132f9bb018ca6738a19f'; // Obtained during registration with this API or using MissVenom (https://github.com/shirioko/MissVenom)to sniff from your phone.
+$nickname = 'John Doe';                         // This is the username displayed by WhatsApp clients.
+
+
+// Create a instance of WhastPort.
+$w = new WhatsProt($username, $identity, $nickname, $debug);
+
+// How to create an account __ONLY__ if you do not have a associated to our phone number.
+// You can test your credentials with: $w->checkCredentials() (BUT ONLY IF NUMBER IS REGISTERED THROUGH WhatsAPI);
+
+/**
+ * First request a registration code from WhatsApp.
+ *
+ * @param $method
+ *   Accepts only 'sms' or 'voice' as a value.
+ * @param $countryCody
+ *   ISO Country Code, 2 Digit.
+ * @param $langCode
+ *   ISO 639-1 Language Code: two-letter codes.
+ *
+ * @return object
+ *   An object with server response.
+ *   - status: Status of the request (sent/fail).
+ *   - reason: Reason of the status (e.g. too_recent/missing_param/bad_param).
+ *   - length: Registration code lenght.
+ *   - method: Used method.
+ *   - retry_after: Waiting time before requesting a new code.
+ */
+$w->codeRequest('sms');
+
+
+// You must wait until you receive a code from Whatsapp, either to your phone via a sms
+// or phonecall depending on what you selected above.
+
+// The function below will only work once you know your code!
+
+
+/**
+ * Second register account on WhatsApp using the provided code with $w->requestCode();.
+ *
+ * @param integer $code
+ *   Numeric code value provided on requestCode().
+ *
+ * @return object
+ *   An object with server response.
+ *   - status: Account status.
+ *   - login: Phone number with country code.
+ *   - pw: Account password.
+ *   - type: Type of account.
+ *   - expiration: Expiration date in UNIX TimeStamp.
+ *   - kind: Kind of account.
+ *   - price: Formated price of account.
+ *   - cost: Decimal amount of account.
+ *   - currency: Currency price of account.
+ *   - price_expiration: Price expiration in UNIX TimeStamp.
+ */
+$w->codeRegister('123456');
+
+// IT IS VERY IMPORTANT THAT YOU NOTE AND KEEP YOUR DETAILS.
+// YOU WILL NEED TO UPDATE THE SCRIPT WITH THE PASSWORD ETC.
+
+//Use exampleFunctional.php to continue with the examples.
