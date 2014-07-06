215495045adda89e4bd5bd50f8ab4a637f98953c
Leif Ferreira <leifsk8er@gmail.com>
2014-07-06 21:09:31 +0200

    contacts.php nots exists

--- examples/exampleFunctional.php
+++ examples/exampleFunctional.php
@@ -1,7 +1,6 @@
 <?php
 set_time_limit(10);
 require_once '../src/whatsprot.class.php';
-// require_once 'contacts.php';
 
 // phone number, deviceIdentity, and name.
 $options = getopt("d::", array("debug::"));
