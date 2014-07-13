921297de5561fd792d8b17c2b6ef32515f671d56
Leif Ferreira <leifsk8er@gmail.com>
2014-07-13 04:53:01 +0200

    Unnecesary deprecated code

--- src/RC4.class.php
+++ src/RC4.class.php
@@ -43,34 +43,3 @@ class rc4
     }
 
 }
-
-// DEPRECATED: WAUTH-1
-//class KeyStream
-//{
-//    private $rc4;
-//    private $key;
-//
-//    public function __construct($key)
-//    {
-//        $this->rc4 = new RC4($key, 256);
-//        $this->key = $key;
-//    }
-//
-//    public function encode($data, $offset, $length, $append = true)
-//    {
-//        $d = $this->rc4->cipher($data, $offset, $length);
-//        $h = substr(hash_hmac('sha1', $d, $this->key, true), 0, 4);
-//        if ($append)
-//            return $d . $h;
-//        else
-//            return $h . $d;
-//    }
-//
-//    public function decode($data, $offset, $length)
-//    {
-//        /* TODO: Hash check */
-//
-//        return $this->rc4->cipher($data, $offset + 4, $length - 4);
-//    }
-//
-//}
