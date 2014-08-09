5dca42a908facadb114abef9e58cb0908991877f
pmuralikrishna111 <pmuralikrishna111@gmail.com>
2014-08-09 14:27:25 +0530

    Adding the Phonenumber for eventtrigger.

--- src/whatsprot.class.php
+++ src/whatsprot.class.php
@@ -2193,7 +2193,7 @@ class WhatsProt
 
             $result = new SyncResult($index, $sync->getAttribute("sync"), $existingUsers, $failedNumbers);
 
-            $this->eventManager()->fireGetSyncResult($result);
+            $this->eventManager()->fireGetSyncResult($this->phoneNumber,$result);
         }
         if ($node->getTag() == "receipt") {
             $this->eventManager()->fireGetReceipt(
